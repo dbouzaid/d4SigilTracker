8d9e7fcd6c9ba85d9ca7e127cd79e2263467d036
Daniel Bouzaid <d.bouzaid@gmail.com>
2025-02-14 10:24:58 +1100

    Added background image. Added mock data of dungeons. Added list view of dungeons.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,38 @@
-import { Text, View } from "react-native";
+import { FlatList, ImageBackground, ScrollView, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { data } from "../constants/nightmare-dungeons";
+
+interface ItemProps {
+  name: string;
+  location: string;
+}
+
+const Item = ({name, location}: ItemProps) => (
+  <View className="p-4 bg-white rounded-lg m-4">
+    <Text className="text-red-950 font-semibold text-lg">{name} + {location}</Text>
+  </View>
+);
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView
+        className="flex-1"
+        edges={["left", "right"]}
+      >
+        <ImageBackground
+          source={require("@/assets/images/D4_Wallpaper.png")}
+          resizeMode="cover"
+          className="flex-1 justify-center"
+        >
+            <FlatList
+              data={data}
+              renderItem={({ item }) => <Item name={item.name} location={item.location} />}
+            >
+
+            </FlatList>
+        </ImageBackground>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
---
+++ constants/nightmare-dungeons.ts
@@ -0,0 +1,102 @@
+export const data = [
+    {
+        "name": "Aldurwood",
+        "location": "Scosglen"
+    },
+    {
+        "name": "Ancient's Lament",
+        "location": "Dry Steppes"
+    },
+    {
+        "name": "Bastion of Faith",
+        "location": "Hawezar"
+    },
+    {
+        "name": "Bloodsoaked Crag",
+        "location": "Fractured Peaks"
+    },
+    {
+        "name": "Caldera Gate",
+        "location": "Kehjistan"
+    },
+    {
+        "name": "Champion's Demise",
+        "location": "Dry Steppes"
+    },
+    {
+        "name": "Charnel House",
+        "location": "Hawezar"
+    },
+    {
+        "name": "Crusader's Cathedral",
+        "location": "Kehjistan"
+    },
+    {
+        "name": "Dead Man's Dredge",
+        "location": "Fractured Peaks"
+    },
+    {
+        "name": "Demon's Wake",
+        "location": "Scosglen"
+    },
+    {
+        "name": "Forgotten Cave",
+        "location": "Dry Steppes"
+    },
+    {
+        "name": "Halls of the Damned",
+        "location": "Kehjistan"
+    },
+    {
+        "name": "Kor Dragan Barracks",
+        "location": "Fractured Peaks"
+    },
+    {
+        "name": "Lost Archives",
+        "location": "Fractured Peaks"
+    },
+    {
+        "name": "Maugan's Works",
+        "location": "Hawezar"
+    },
+    {
+        "name": "Mercy's Reach",
+        "location": "Fractured Peaks"
+    },
+    {
+        "name": "Path of the Blind",
+        "location": "Scosglen"
+    },
+    {
+        "name": "Penumbral Depths",
+        "location": "Hawezar"
+    },
+    {
+        "name": "Renegade's Retreat",
+        "location": "Dry Steppes"
+    },
+    {
+        "name": "Sepulcher of the Forsworn",
+        "location": "Kehjistan"
+    },
+    {
+        "name": "Shifting City",
+        "location": "Scosglen"
+    },
+    {
+        "name": "Sunken Ruins",
+        "location": "Hawezar"
+    },
+    {
+        "name": "The Onyx Watchtower",
+        "location": "Dry Steppes"
+    },
+    {
+        "name": "Tomb of the Saints",
+        "location": "Kehjistan"
+    },
+    {
+        "name": "Whispering Vault",
+        "location": "Scosglen"
+    }
+];
